235a0bb80317611931f69ee1564d6bd81fdfad73
dhanushk56 <dhanushsatyapawankarri@gmail.com>
2026-09-24 18:44:42 -0500

    Add dark mode support to Footer component

--- components/Footer.js
+++ components/Footer.js
@@ -1,3 +1,3 @@
 export default function Footer() {
-  return <footer className="px-6 md:px-16 py-8 border-t border-black/10 text-center text-xs text-black/40">© 2026 Mickey Chan. All rights reserved.</footer>;
+  return <footer className="px-6 md:px-16 py-8 border-t border-black/10 dark:border-white/10 text-center text-xs text-black/40 dark:text-white/40">© 2026 Mickey Chan. All rights reserved.</footer>;
 }
